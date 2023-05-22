b6c5d5b191647a64022dad244d13f28270ce38a8
Dales10 <matiasdario75@gmail.com>
2023-05-22 16:39:24 -0300

    Product table routes created.

--- prisma/migrations/20230522164722_init/migration.sql
+++ prisma/migrations/20230522164722_init/migration.sql
@@ -1,50 +1,59 @@
 -- CreateTable
 CREATE TABLE "user" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "birthdate" DATETIME NOT NULL,
+    "birthdate" TIMESTAMP(3) NOT NULL,
     "phone" TEXT NOT NULL,
     "address" TEXT NOT NULL,
     "city" TEXT NOT NULL,
     "cep" TEXT NOT NULL,
     "state" TEXT NOT NULL,
-    "country" TEXT NOT NULL
+    "country" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "category" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "category_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "product" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "price" INTEGER NOT NULL,
-    "categoryId" TEXT NOT NULL
+    "price" TEXT NOT NULL,
+    "categoryId" TEXT[],
+
+    CONSTRAINT "product_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "order" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
-    "orderDay" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "totalAmount" REAL NOT NULL
+    "orderDay" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "totalAmount" DOUBLE PRECISION NOT NULL,
+
+    CONSTRAINT "order_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "orderItem" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "productId" TEXT NOT NULL,
     "quantity" INTEGER NOT NULL,
-    "unitPrice" REAL NOT NULL,
+    "unitPrice" DOUBLE PRECISION NOT NULL,
     "orderId" TEXT NOT NULL,
-    CONSTRAINT "orderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "order" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+
+    CONSTRAINT "orderItem_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -58,3 +67,6 @@ CREATE UNIQUE INDEX "category_name_key" ON "category"("name");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "product_name_key" ON "product"("name");
+
+-- AddForeignKey
+ALTER TABLE "orderItem" ADD CONSTRAINT "orderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/routes/products.ts
+++ src/routes/products.ts
@@ -4,8 +4,78 @@ import * as z from "zod";
 import prisma from "../lib/prisma";
 
 const productRoutes = async (server: FastifyInstance) => {
-    server.get('/product', async () => {
-        return "products";
+    server.get('/product/:id', async (request) => {
+        const createProductParams = z.object({
+            id: z.string().uuid(),
+        });
+
+        const id = createProductParams.parse(request.params);
+
+        const product = await prisma.products.findUnique({
+            where: id,
+        });
+
+        return product;
+    });
+
+    server.get("/products", async () => {
+        const products = await prisma.products.findMany();
+
+        return products;
+    });
+
+    server.post("/product", async (request) => {
+        const createProductBody = z.object({
+            name: z.string().min(4).max(30),
+            description: z.string().min(20).max(500),
+            price: z.string().min(1).max(6),
+            categoryId: z.string().array().min(1).max(10),
+        });
+
+        const data = createProductBody.parse(request.body);
+
+        await prisma.products.create({
+            data,
+        });
+
+        return "success";
+    });
+
+    server.patch("/product/:id", async (request) => {
+        const createProductParams = z.object({
+            id: z.string().uuid(),
+        });
+
+        const createProductBody = z.object({
+            name: z.string().min(4).max(30).optional(),
+            description: z.string().min(20).max(500).optional(),
+            price: z.string().min(1).max(6).optional(),
+            categoryId: z.string().array().min(1).max(10).optional(),
+        });
+
+        const id = createProductParams.parse(request.params);
+        const data = createProductBody.parse(request.body);
+
+        await prisma.products.update({
+            where: id,
+            data,
+        });
+
+        return "success";
+    });
+
+    server.delete("/product/:id", async (request) => {
+        const createProductParams = z.object({
+            id: z.string().uuid(),
+        });
+
+        const id = createProductParams.parse(request.params);
+
+        await prisma.products.delete({
+            where: id,
+        });
+
+        return "success";
     });
 };
 
